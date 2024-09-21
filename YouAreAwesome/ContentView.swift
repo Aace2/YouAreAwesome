920b00e6c8b39365d4ba9170ef74650db7e86247
Aaron Anderson <aace2@me.com>
2024-09-21 08:01:34 -0700

    11

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,16 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
+    
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(.rect(cornerRadius: 30))
+                .padding(30)
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .padding()
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            
+            Button("Motivate") {
+                let messages = ["Dream BIG!",
+                                "Momentum Brings Clarity",
+                                "You Are Limitless",
+                                "Create The Future",
+                                "Develop A Growth Mindset",
+                                "Embrace The Work",
+                                "Be Disciplined",
+                                "Be Consistent"
+                ]
+                
+                messageString = messages[messageNumber]
+                messageNumber += 1
+                messageNumber = messageNumber == messages.count ? 0 : messageNumber
+                
+                imageName = "image\(imageNumber)"
+                imageNumber += 1
+                imageNumber = imageNumber > 15 ? 0 : imageNumber
+            }
+            .font(.title2)
+            .fontWeight(.heavy)
+            .buttonStyle(.borderedProminent)
+            .padding()
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
